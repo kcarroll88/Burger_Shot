7b29f5b8bb5599f3e89df94d26f16b165d137d74
Keenan <keenan.carroll.va@gmail.com>
2019-03-23 08:42:14 -0400

    Player moving limits

--- Burger Shot/Assets/Scripts/Player.cs
+++ Burger Shot/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,15 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField] float moveSpeed = 10f;
 
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBondaries();
     }
 
     // Update is called once per frame
@@ -22,10 +28,21 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBondaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).y;
     }
 }
