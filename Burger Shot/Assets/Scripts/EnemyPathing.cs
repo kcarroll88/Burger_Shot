e07d33900098e7e826afce97a7554504f53afa49
Keenan <keenan.carroll.va@gmail.com>
2019-03-24 07:45:30 -0400

    Added enemy pathing

--- Burger Shot/Assets/Scripts/EnemyPathing.cs
+++ Burger Shot/Assets/Scripts/EnemyPathing.cs
@@ -6,16 +6,29 @@ public class EnemyPathing : MonoBehaviour
 {
     // Configs
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
     }
 }
