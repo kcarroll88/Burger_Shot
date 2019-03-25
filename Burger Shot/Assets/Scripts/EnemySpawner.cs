53baa558034505ccbc957dd903e269af0ddc0f0a
Keenan <keenan.carroll.va@gmail.com>
2019-03-25 08:56:00 -0400

    Setting up Waves and Enemy Spawns

--- Burger Shot/Assets/Scripts/EnemySpawner.cs
+++ Burger Shot/Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] List<WaveConfig> waveConfigs;
+
     // Start is called before the first frame update
     void Start()
     {
