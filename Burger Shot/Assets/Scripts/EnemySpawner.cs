9c30e38c9302008cd5bfb109ac23c2800bde2dfc
Keenan <keenan.carroll.va@gmail.com>
2019-03-25 12:33:18 -0400

    Fix on enemy prefab
    Enemy wasn't getting waypoints

--- Burger Shot/Assets/Scripts/EnemySpawner.cs
+++ Burger Shot/Assets/Scripts/EnemySpawner.cs
@@ -6,15 +6,18 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
 
+    int startingWave = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
-        
+        Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
     }
 }
