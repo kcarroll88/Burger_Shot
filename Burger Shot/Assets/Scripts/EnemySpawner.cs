5a78507f93f1d4921322a2213db7f790989861e8
Keenan <keenan.carroll.va@gmail.com>
2019-03-26 11:49:23 -0400

    for loop to spawn multiple enemies

--- Burger Shot/Assets/Scripts/EnemySpawner.cs
+++ Burger Shot/Assets/Scripts/EnemySpawner.cs
@@ -6,7 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
 
-    int startingWave = 1;
+    int startingWave = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,10 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
-        Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        for (int enemyCount = 0; enemyCount <= waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
     }
 }
